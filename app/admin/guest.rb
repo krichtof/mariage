963308db052a4c0e2b3b3f907c0193743e6b0388
Christophe Robillard <geek@robiweb.net>
2015-03-22 21:40:24 +0100

    update admin guest

--- app/admin/guest.rb
+++ app/admin/guest.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register Guest do
-
-
-
+  controller do
+    # Overriding resource url helpers so that this admin controller uses the id
+    # for urls, rather than the slug (which can have duplicates):
+    def resource_path(*given_args)
+      given_options = given_args.extract_options!
+      admin_guest_path((given_args.first || @guest).id, given_options)
+    end
+    def edit_resource_path(*given_args)
+      given_options = given_args.extract_options!
+      edit_admin_guest_path((given_args.first || @guest).id, given_options)
+    end
+    def resource_url(*given_args)
+      given_options = given_args.extract_options!
+      admin_guest_url((given_args.first || @guest).id, given_options)
+    end
+  end
 end
